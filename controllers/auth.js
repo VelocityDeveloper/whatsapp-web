8b0edc9c7c1c946ade8627335a71c46edf4f8f44
aadiityaak <aadiityaak@gmail.com>
2022-09-27 13:45:15 +0700

    berhasil menyimpan refreshToken

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,23 +45,43 @@ exports.register = (req, res) => {
 
 }
 
-exports.login = (req, res) => {
+exports.login = async (req, res) => {
     const { email, password } = req.body;
+    
     db.query('SELECT * FROM users WHERE email = ?', [email], async (error, results) => {
+        const userId = results[0].id;
+        const userName = results[0].name;
+        const userEmail = results[0].email;
+        const userPassword = results[0].password;
         if(error) {
             console.log(error);
         }
 
-        // console.log(email);
-        // console.log(password);
-
-        if(email !== results[0].email){
+        if(email !== userEmail){
             return res.render('login', {
                 message: 'Email tidak terdafatar'
             });
         }
 
-        if(bcrypt.compare(password, results[0].email)){
+        if(await bcrypt.compare(password, userPassword)){
+
+            const accessToken = jwt.sign({userId, userName, userEmail}, process.env.ACCESS_TOKEN,{
+                expiresIn: '20s'
+            });
+            const refreshToken = jwt.sign({userId, userName, userEmail}, process.env.ACCESS_TOKEN,{
+                expiresIn: '1d'
+            });
+            // await Users.update({refresh_token: refreshToken},{
+            //     where:{
+            //         id: userId
+            //     }
+            // });
+            res.cookie('refreshToken', refreshToken,{
+                httpOnly: true,
+                maxAge: 24 * 60 * 60 * 1000
+            });
+            // res.json({ accessToken });
+
             return res.render('login', {
                 message: 'Login berhasil!'
             });
